86a63495fc2738783fdd30018a7001f53b8815b8
andresni <andresni@pk-ga1-105a.uio.no>
2016-11-14 11:25:18 +0100

    This is the initial commit: v.01
    Most of it should be working.
    Missing features:
    Textfile reporting?
    Querying for paths
    Auto cleaning
    Some more function deleniation
    templates for functions and csvs
    filesaving structure

---
+++ eeganalysis.m
@@ -0,0 +1,44 @@
+function [] = eeganalysis( csvname )
+%EEGANALYSIS Main analysis script. Calls stuff
+%and reads stuff.
+%By André Sevenius Nilsen & Benjamin Thürer
+% sevenius.nilsen@gmail.com
+% benjamin.thuerer@kit.edu
+
+%%Calling the read csv file function
+param=readcsv(csvname);
+
+fldnames1 = fieldnames(param);
+
+for sbj = 1:size(fldnames1,1)
+    for ses = 1:size(fieldnames(eval(['param.' fldnames1{sbj}])),1)
+        fldnames2 = fieldnames(eval(['param.' fldnames1{sbj}]));
+        fldnames3 = fieldnames(eval(['param.' fldnames1{sbj} '.' fldnames2{ses}]));
+        idx_src1 = find(strcmp(fldnames3,'vhdrsource'));
+        idx_src2 = find(strcmp(fldnames3,'downsample_rate'));
+        
+        data_struct = eval(['param.' fldnames1{sbj} '.' fldnames2{ses}]);
+        
+        si_edge = idx_src2-idx_src1-1;
+        si = 1;
+        n = zeros(si_edge,1);
+        while si < si_edge+1
+            n(si) = str2num(eval(['param.' fldnames1{sbj} '.' fldnames2{ses} '.' fldnames3{si+idx_src1}]));
+            si = si+1;
+        end
+        
+        t = 1;
+        locFile = [];
+        EEG = [];
+        while 1
+            if isempty(find(n==t))
+                break
+            end
+            [EEG,locFile] = feval(fldnames3{find(n==t)+idx_src1},data_struct,fldnames1(sbj),EEG,locFile);
+            t = t+1;
+        end
+    end
+end
+
+end
+
---
+++ private/UiO_save.m
@@ -0,0 +1,82 @@
+% EEG-data processing for EEG-TMS combined
+% Consciousness Study Oslo
+% 
+% [EEG,locFile] = UiO_save(data_struct,subj_name,EEG,locFile)
+% 
+% data_struct: structure of the csv-file specified for subject and
+%               experiment
+% subj_name: subject name to store the file
+% EEG: EEG structure of previous function.
+% locFile: locFile of previous function.
+% 
+% This function will save the eeg file and the loc file as .mat in the raw
+% data folder (if save_folder: 0 ) or in the provided folder path
+% (save_folder)
+% 
+%
+% by questions: benjamin.thuerer@kit.edu
+% 
+function [EEG,locFile] = UiO_save(data_struct,subj_name,EEG,locFile)
+
+if nargin < 4
+    error('this function needs data structure, subject name, processed EEG data, and locFile')
+elseif isempty(EEG)
+    error('this function needs processed EEG data. Run a processing step before this')
+end
+
+ending_name = locFile{end};
+endName = ending_name{1};
+
+% create file and loc name
+save_name = [subj_name{1} '_' data_struct.session '_' endName];
+loc_name = [subj_name{1} '_' data_struct.session '_locFile'];
+
+% save file and loc to the same folder as the raw data if no save_folder
+% path provided in csvfile
+if str2double(data_struct.save_folder) == 0
+    if isempty(strfind(data_struct.vhdrsource,'\'))
+        char_idx = strfind(data_struct.vhdrsource,'/'); 
+    else
+        char_idx = strfind(data_struct.vhdrsource,'\');
+    end
+    data_path = data_struct.vhdrsource(1:char_idx(end));
+    save_file = [data_path  save_name];
+    save_loc = [data_path loc_name];
+else
+    % if save_folder path provided, check if / or \ is used and save file
+    % and loc-file. If subfolders (name, session) are nonexistent, create
+    % them
+    subfolder1 = subj_name{1};
+    subfolder2 = data_struct.session;
+    
+    if isempty(strfind(data_struct.save_folder,'\'))
+        save_folder1 = [data_struct.save_folder '/' subfolder1];
+        save_folder2 = [data_struct.save_folder '/' subfolder1 '/' subfolder2];
+        if ~exist(save_folder1, 'dir')
+          mkdir(save_folder1);
+        end
+        if ~exist(save_folder2, 'dir')
+          mkdir(save_folder2);
+        end
+        save_file = [save_folder2 '/' save_name];
+        save_loc = [save_folder2 '/' loc_name]; 
+    else
+        save_folder1 = [data_struct.save_folder '\' subfolder1];
+        save_folder2 = [data_struct.save_folder '\' subfolder1 '\' subfolder2];
+        if ~exist(save_folder1, 'dir')
+          mkdir(save_folder1);
+        end
+        if ~exist(save_folder2, 'dir')
+          mkdir(save_folder2);
+        end
+        save_file = [save_folder2 '\' save_name];
+        save_loc = [save_folder2 '\' loc_name];
+    end    
+end
+
+EEG.data = single(EEG.data);
+save([save_file '.mat'],'EEG','-v7.3');
+save([save_loc '.mat'],'locFile');
+
+
+end
